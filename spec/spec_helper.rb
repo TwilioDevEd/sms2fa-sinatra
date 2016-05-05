980458c02e45fd0e7011e9d82b086e2890e7e29e
Agustin Camino <agustin.camino@gmail.com>
2016-05-05 18:07:29 -0500

    Configure database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,17 @@ require_relative File.join('..', 'app')
 RSpec.configure do |config|
   include Rack::Test::Methods
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   def app
     TwoFactorAuth::App
   end
