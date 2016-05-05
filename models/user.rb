5d842a6ae6ceb5a9f4c16231ab29481e2b236520
Agustin Camino <agustin.camino@gmail.com>
2016-05-05 18:09:06 -0500

    Add user model

---
+++ models/user.rb
@@ -0,0 +1,20 @@
+require 'data_mapper'
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name,        String,     required: true
+  property :last_name,         String,     required: true
+  property :email,             String,     required: true
+  property :phone_number,      String,     required: true
+  property :password,          BCryptHash, required: true
+  property :verification_code, String,                     default:  ''
+  property :confirmed,         Boolean,                    default:  false
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email,
+                        :phone_number,
+                        :password
+end
