86acb9720e3df18990b546878ee4fa71773b3e3a
Agustin Camino <agustin.camino@gmail.com>
2016-05-06 07:26:00 -0500

    Add confirmation sender

---
+++ spec/lib/confirmation_sender_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../spec_helper'
+
+require_relative '../../lib/confirmation_sender'
+require_relative '../../lib/code_generator'
+require_relative '../../lib/message_sender'
+
+describe ConfirmationSender do
+  describe '.send_confirmation_to' do
+    let(:user) do
+      User.create(first_name:   'Alice',
+                  last_name:    'Doe',
+                  email:        'alice@example.com',
+                  phone_number: '555-555',
+                  password:     's3cr37')
+    end
+    let(:verification_code) { '219023' }
+
+    before do
+      allow(CodeGenerator).to receive(:generate) { verification_code }
+      allow(MessageSender).to receive(:send_code)
+        .with(user.phone_number, verification_code )
+      described_class.send_confirmation_to(user)
+    end
+
+    it 'generates a code' do
+      expect(CodeGenerator).to have_received(:generate).once
+    end
+
+    it 'updates the verification code' do
+      expect(user.verification_code).to eq(verification_code)
+    end
+
+    it 'sends the generated code' do
+      expect(MessageSender).to have_received(:send_code)
+        .with(user.phone_number, verification_code).once
+    end
+  end
+end
